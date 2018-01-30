3d4ad6ed85ec30acb6e99f6252bdc821ab61922a
jalonenrichard <32843817+jalonenrichard@users.noreply.github.com>
2018-01-31 00:03:42 +0200

    Create a pie shop with .net core 2 MVC
    https://app.pluralsight.com/library/courses/aspdotnet-core-2-0-mvc-application-visual-studio-2017/table-of-contents

---
+++ PluralsightPieShop/PluralsightPieShop/Models/PieRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PluralsightPieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        public PieRepository(AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _appDbContext.Pies;
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _appDbContext.Pies.FirstOrDefault(p => p.Id == pieId);
+        }
+    }
+}
---
+++ PluralsightPieShop/PluralsightPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PluralsightPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PluralsightPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
